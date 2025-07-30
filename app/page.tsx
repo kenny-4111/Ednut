de8f7693d9fa496aabae5ce63edad7366b61e414
Kenny-4111 <oluyoleprovidence@gmail.com>
2025-07-30 19:55:49 +0100

    final one

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
         <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
           <div className="max-w-md space-y-4">
             <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
-              Welcome to kennys shop
+              Welcome to kendall store
             </h2>
             <p className="text-neutral-600">
               Discover the latest products at the best prices
